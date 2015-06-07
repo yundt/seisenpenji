2164e9a0c3bb70485027439c45f28588eef95bd7
Marek Nowak <mareknowakk@gmail.com>
2015-06-07 12:09:51 +0200

    Inits?

--- core/built/assets/ghost.js
+++ core/built/assets/ghost.js
@@ -24415,7 +24415,7 @@ catch(err) {
 if (runningTests) {
   require("ghost/tests/test-helper");
 } else {
-  require("ghost/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ghost","version":"0.0.0.202d90e0"});
+  require("ghost/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ghost","version":"0.0.0."});
 }
 
 /* jshint ignore:end */
