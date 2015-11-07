c66c667c14acf7aad2ca68865601d80577c58730
Marek Nowak <mareknowakk@gmail.com>
2015-11-07 23:47:04 +0100

    Runs grunt init?

--- core/built/assets/ghost.js
+++ core/built/assets/ghost.js
@@ -24415,7 +24415,7 @@ catch(err) {
 if (runningTests) {
   require("ghost/tests/test-helper");
 } else {
-  require("ghost/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ghost","version":"0.0.0."});
+  require("ghost/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ghost","version":"0.0.0.aa92acb3"});
 }
 
 /* jshint ignore:end */
